2e35da6f8de00af45ee17b4267f369b2a781b64b
ThummalaChakravarthi2 <thummalachakravarthi2002@gmail.com>
2025-10-06 12:22:00 +0530

    adding viewalljobs in controller

--- src/main/java/com/chakri/springweb2/HomeController.java
+++ src/main/java/com/chakri/springweb2/HomeController.java
@@ -1,13 +1,21 @@
 package com.chakri.springweb2;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.chakri.springweb2.service.jobService;
+
 import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
+	@Autowired
+    private jobService service; 
 
     @GetMapping({"/","/home"})
     public String home() {
@@ -27,6 +35,14 @@ public class HomeController {
         model.addAttribute("JobPost", jp);
 
         System.out.println("Form submitted: " + jp);
+        service.addJob(jp);
         return "success"; // forwards to success.jsp
+    }
+    @GetMapping("/viewalljobs")
+    public String viewJobs(Model m) {
+        System.out.println("View all jobs method called");
+        List<JobPost> jobs = service.getALLJobs();
+        m.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
     }
 }
--- src/main/java/com/chakri/springweb2/service/jobService.java
+++ src/main/java/com/chakri/springweb2/service/jobService.java
@@ -3,10 +3,11 @@ package com.chakri.springweb2.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.chakri.springweb2.JobPost;
 import com.chakri.springweb2.jobrepo.JobRepo;
-
+@Service
 public class jobService {
 	@Autowired
 	private JobRepo repo;
